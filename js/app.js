6043e3de19c02640a29468a13df1663fa23de832
huangrkul <huangrkul@gmail.com>
2019-09-23 16:35:27 -0700

    event listener implemented and working

--- js/app.js
+++ js/app.js
@@ -6,9 +6,10 @@
 // var imgBox = document.getElementById('imgsContainer');
 var imgList = document.getElementById('imgsList');
 
-//create li and img elements to handle dynamic amount of displayed products.
+//global variables that will be used later.
 var liEl = null;
 var imgEl = null;
+var priorProducts = [];
 
 //actual images in an Array
 var objArray = [
@@ -35,12 +36,13 @@ var objArray = [
 ];
 
 var prodDisplayNumbers = 3; //how many images need to be generated;
-var prodImgArray = []; //this holds all the img tags created dynamically.
 
 //uppercase first letter in constructor name!!!!
 function Products (name, ext) {
   this.name = name;
   this.image = 'img/' + name + ext;
+  this.views = 0;
+  this.clicked = 0;
   //push all instants into an array.  (instead of giving name to each object)
   Products.allProds.push(this);
 }
@@ -53,29 +55,42 @@ function randomProd() {
   var randNum = Math.floor(Math.random() * Products.allProds.length);
   return randNum;
 }
+
 //function to render the randomly picked products to img tags.
 function renderProds() {
   //this holds the 3 unique random numbers
   var randArray = [];
+  var prodImgArray = [];
   var randIndex = null;
 
+  //copy randArray values into priorProducts Array.
+
+  //generate the li and img for the amount of prodDisplayNumbers
+  for(var j=0; j < prodDisplayNumbers; j++){
+    liEl = document.createElement('li');
+    imgEl = document.createElement('img');
+    imgEl.addEventListener('click', imgClickHandler);
+    liEl.appendChild(imgEl);
+    imgList.appendChild(liEl);
+    prodImgArray.push(imgEl);
+  }
   /*the do while loop does the following in order:
-    1. If the randArray is empty, generate the first random number.
-    2. If not, generate a random number and evaluate it with existing numbers in the randArray.
-    3. During the evaluation, if the number matches existing number in array, isRepeated = true.
-    4. If not, isRepeated remains false.
-    5. If the evaluation returns a false, which means the number is unique, therefore, can be pushed into the array.
-    6. Repeat step 1 to 5 while the length of the randArray is less than prodDisplayNumbers.
-    */
+  1. If the priorProducts Array is empty, generate the first random number.
+  2. If not, generate a random number and evaluate it with existing numbers in the randArray AND priorProducts Array
+  3. During the evaluation, if the number matches existing number in arrays, isRepeated = true.
+  4. If not, isRepeated remains false.
+  5. If the evaluation returns a false, which means the number is unique, therefore, can be pushed into the array.
+  6. Repeat step 1 to 5 while the length of the randArray is less than prodDisplayNumbers.
+  */
   do {
-    if(randArray.length === 0) {
+    if(priorProducts.length === 0) {
       randIndex = randomProd();
       randArray.push(randIndex);
     } else {
       randIndex = randomProd();
       var isRepeated = false;
-      for(var i=0; i < randArray.length; i++) {
-        if(randIndex === randArray[i]) {
+      for(var i=0; i < priorProducts.length; i++) {
+        if(randIndex === randArray[i] || randIndex === priorProducts[i]) {
           isRepeated = true;
         }
       }
@@ -85,24 +100,35 @@ function renderProds() {
     }
   } while (randArray.length < prodDisplayNumbers);
 
-  for (var k=0; k < randArray; k++) {
+  //finally, attach each associated object.image to img.src
+  for (var k=0; k < randArray.length; k++) {
     var products = Products.allProds;
+    prodImgArray[k].id = products[randArray[k]].name;
     prodImgArray[k].src = products[randArray[k]].image;
+    products[randArray[k]].views++;
     console.log(products[randArray[k]]);
   }
+
+  priorProducts = randArray.slice(0);
+  console.log(priorProducts);
+}
+
+function imgClickHandler(event) {
+  console.log(event.target);
+  for (var i=0; i < Products.allProds.length; i++){
+    if(event.target.id === Products.allProds[i].name) {
+      Products.allProds[i].clicked++;
+      console.log(Products.allProds[i].clicked);
+    }
+  }
+  imgList.innerHTML = '';
+  renderProds();
 }
 
 
 function init() {
   for(var i=0; i < objArray.length; i++){
     new Products(objArray[i][0], objArray[i][1]);
-  }
-  for(var j=0; j < prodDisplayNumbers; j++){
-    liEl = document.createElement('li');
-    imgEl = document.createElement('img');
-    liEl.appendChild(imgEl);
-    imgList.appendChild(liEl);
-    prodImgArray.push(imgEl);
   }
   renderProds();
 }
