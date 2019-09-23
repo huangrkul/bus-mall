c8afe802e17e67579ad0ad2481342ec8602bf199
huangrkul <huangrkul@gmail.com>
2019-09-23 12:12:40 -0700

    clearing user_stories

---
+++ js/app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var nullVar = null; //start null then add value later
+
+//uppercase first letter in constructor name!!!!
+function SomeCons (name, image) {
+  this.name = name;
+  this.image = image;
+  //push all instants into an array.  (instead of giving name to each object)
+  SomeCons.allImgs.push(this);
+}
+
+//this returns a random number between 0 and allImgs length.
+function randomImg() {
+  var randNum = Math.floor(Math.random() * SomeCons.allImgs.length);
+  return randNum;
+}
+
+//create do while loop in image rendering logic: do it once, if same numbers, do it again.
+function renderImg() {
+  do{
+    //generate random numbers
+  }while(//check if numbers are same);
+}
+
+//a property in constructor function that holds an array of all the instants.
+SomeCons.allImgs = [];
